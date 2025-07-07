0e28f2943f812c756a6b65f81accb0b77045cb79
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2025-07-07 20:34:46 +0530

    Create handshake.py

---
+++ modules/handshake.py
@@ -0,0 +1,6 @@
+from utils.helpers import colorful_print
+
+def start():
+    colorful_print("Starting handshake capture module...", "cyan")
+    # Placeholder for actual handshake capture logic
+    print("This feature will help you capture WPA/WPA2 handshakes. (To be implemented)")
