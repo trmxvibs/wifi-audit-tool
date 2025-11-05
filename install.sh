4b46f6f0c1dc86877f328cdd0d1ec47178c7101d
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2025-11-05 10:49:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "Installing dependencies..."
-pkg install -y python aircrack-ng git
-pip install -r requirements.txt
-echo "Done! Run: python wifi_audit.py"
+
+echo "Installing dependencies for Kali Linux..."
+
+sudo apt-get update
+
+
+sudo apt-get install -y python3-pip aircrack-ng git
+
+
+pip3 install -r requirements.txt
+
+echo "Done! Run: sudo python3 wifi_audit.py"
